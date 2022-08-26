f66810e2cd83942068580d99044936726c14836c
Vaibhav Kadam <vaibhav02kadam@gmail.com>
2022-08-26 13:13:44 -0400

    added victor seirra pattern

--- getting_started/solution/turtle_graphics_star.py
+++ getting_started/solution/turtle_graphics_star.py
@@ -1,10 +1,10 @@
 import turtle as tr
 
-tr.color('green', 'orange')
-tr.begin_fill()
+tr.color('green', 'yellow')
 triangle_count = 0
 search_count = 0
 leg_count = 0
+tr.begin_fill()
 
 while True:
     tr.forward(200)  #Moves turtle forward 
@@ -19,14 +19,13 @@ while True:
         
     if abs(tr.pos()) < 1 and triangle_count == 3:
         triangle_count = 0
-      
+
         if abs(tr.pos()) < 1 and search_count < 3:
             tr.right(30)
             search_count += 1
 
     if search_count == 3:
         break
-
 
 tr.end_fill()
 tr.done()
